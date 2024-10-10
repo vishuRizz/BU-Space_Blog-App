854f5450b771cbdae93f88e36f4040d65cb754f7
vishu pratap <vishurizz01@gmail.com>
2024-10-10 12:13:36 +0530

    adding frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-red-500 ">Typescript + React</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
